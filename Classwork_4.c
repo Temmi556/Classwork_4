c1d34b0233276d80ea9221c688d74217df95e4d4
Temmi556 <temmi556@mail.ru>
2025-10-03 16:39:23 +0300

    Initial README.md

---
+++ Classwork_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int maint(){
+    char c = '!'; 
+
+    int i = 2;
+
+    float f = 3.14f;
+
+    double d= 5e-12;
+
+    printf("%c\n", c);
+    printf("%d\n", i);
+    printf("%f\n", f);
+    printf("%lf\n", d);
+
+}
